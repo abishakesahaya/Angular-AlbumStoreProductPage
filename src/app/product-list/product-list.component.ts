727e5ed53b943aaa1c2ff7346d863474a4580ac1
abishakesahaya <77775854+abishakesahaya@users.noreply.github.com>
2021-01-22 12:26:33 +0530

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductListComponent implements OnInit {
   products:Product[];
-  constructor(private __productService:ProductService) { }
+  constructor(private _productService:ProductService) { }
 
   ngOnInit() {
   }
